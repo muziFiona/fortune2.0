8de7d86f16cd77b3759080a708ab4fa885283cb8
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-06-10 23:01:12 +0100

    Add files via upload

---
+++ RUNES.js
@@ -0,0 +1,70 @@
+//this will be the script for the Runes structure, drawing, meaning, the results
+
+let runes = [];
+let index ;
+
+
+function LETTERS(){
+  fill(10, 0, 0, op);
+  textFont(myFont);
+  textSize(32);
+  textAlign(LEFT,TOP);
+  //text(index, 100,100);
+  text(runes[index],-160,-100);
+  textImg();
+}
+
+function textImg(){
+      if (runes[index]==runes[0]){
+      rI= runesImg[1];
+    }else if (runes[index]==runes[1]){
+      rI= runesImg[2];
+    }else if (runes[index]==runes[2]){
+      rI= runesImg[3];
+    }else if (runes[index]==runes[3]){
+      rI= runesImg[4];
+    }else if (runes[index]==runes[4]){
+      rI= runesImg[5];
+    }else if (runes[index]==runes[5]){
+      rI= runesImg[6];
+    }else if (runes[index]==runes[6]){
+      rI= runesImg[7];
+    }else if (runes[index]==runes[7]){
+      rI= runesImg[8];
+    }else if (runes[index]==runes[8]){
+      rI= runesImg[9];
+    }else if (runes[index]==runes[9]){
+      rI= runesImg[10];
+    }else if (runes[index]==runes[10]){
+      rI= runesImg[11];
+    }else if (runes[index]==runes[11]){
+      rI= runesImg[12];
+    }else if (runes[index]==runes[12]){
+      rI= runesImg[13];
+    }else if (runes[index]==runes[13]){
+      rI= runesImg[14];
+    }else if (runes[index]==runes[14]){
+      rI= runesImg[15];
+    }else if (runes[index]==runes[15]){
+      rI= runesImg[16];
+    }else if (runes[index]==runes[16]){
+      rI= runesImg[17];
+    }else if (runes[index]==runes[17]){
+      rI= runesImg[18];
+    }else if (runes[index]==runes[18]){
+      rI= runesImg[19];
+    }else if (runes[index]==runes[19]){
+      rI= runesImg[20];
+    }else if (runes[index]==runes[20]){
+      rI= runesImg[21];
+    }else if (runes[index]==runes[21]){
+      rI= runesImg[22];
+    }else if (runes[index]==runes[22]){
+      rI= runesImg[23];
+    }else if (runes[index]==runes[23]){
+      rI= runesImg[24];
+    }else if (runes[index]==runes[24]){
+      rI= runesImg[25];
+    }
+
+}
